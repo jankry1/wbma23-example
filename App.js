1d3b824db25fb0b34635fba468325f7cb6657a52
Yana Krylova <yana.krylova@outlook.com>
2023-01-10 14:02:06 +0200

    add components, flexbox layout missing

--- App.js
+++ App.js
@@ -1,30 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import List from './components/List.js';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={mediaArray}
-        renderItem = {(item) => {
-          return (
-            <TouchableOpacity>
-              <Image
-              style={{width:100, height:100}}
-              source={{uri: item.thumbnail}}
-              />
-              <View>
-                <Text>{item.title}</Text>
-                <Text>{item.description}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-        />
+    <>
+      <SafeAreaView style={styles.container}>
+        <List />
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
-}
+};
 
 export default App;
 
