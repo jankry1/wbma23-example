c5bb272d2a2b001047e90b231e0634d80e40dc01
Yana Krylova <yana.krylova@outlook.com>
2023-01-13 12:24:11 +0200

    add tab icons

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => {
+        return {
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'person';
+            }
+
+            return <Ionicons name={iconName} />;
+          },
+        };
+      }}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Single" component={Single} />
     </Tab.Navigator>
   );
 };
