4d6e2611b854c1fccb302e672a7b117071b81a4d
Yana Krylova <yana.krylova@outlook.com>
2023-02-06 13:38:57 +0100

    package.json updated

--- views/Profile.js
+++ views/Profile.js
@@ -1,28 +1,42 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {MainContext} from '../contexts/MainContext';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
+import {Card, Icon, ListItem} from '@rneui/themed';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {user} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('');
+
+  const loadAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+      setAvatar(avatarArray.pop().filename);
+    } catch (error) {
+      console.log('user avatar fetch failed', error.message);
+    }
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Profile</Text>
-      <Text>Username: {user.username}</Text>
-      <Text>Email: {user.email}</Text>
-      <Text>Full name: {user.full_name}</Text>
-    </SafeAreaView>
+    <Card>
+      <Card.Title>Username: {user.username}</Card.Title>
+      <Card.Image source={{uri: uploadsUrl + avatar}}></Card.Image>
+      <ListItem>
+        <Icon name="email"></Icon>
+        <ListItem.Title>{user.email}</ListItem.Title>
+      </ListItem>
+      <ListItem>
+        <Icon name="badge"></Icon>
+        <ListItem.Title>{user.full_name}</ListItem.Title>
+      </ListItem>
+    </Card>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#77A6F7',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 40,
-  },
-});
 
 export default Profile;
