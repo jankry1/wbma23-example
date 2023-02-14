43ee4f23cb8b1462d030f3b4f5ad017037027482
Yana Krylova <yana.krylova@outlook.com>
2023-02-14 11:46:03 +0200

    fixed uploading

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -2,49 +2,40 @@ import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
-import Settings from '../views/Settings';
 import Login from '../views/Login';
 import {MainContext} from '../contexts/MainContext';
+import Upload from '../views/Upload';
+import {Icon} from '@rneui/themed';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => {
-        return {
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            if (route.name === 'Settings') {
-              iconName = 'settings';
-            } else if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Profile') {
-              iconName = 'person';
-            }
-            return <Ionicons name={iconName} />;
-          },
-          tabBarStyle: {
-            backgroundColor: '#D3E3FC',
-          },
-        };
-      }}
-    >
-      <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
+    <Tab.Navigator>
       <Tab.Screen
-        name="Settings"
-        component={Settings}
-        options={{headerShown: false}}
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="home" color={color} />,
+        }}
+      />
+      <Tab.Screen
+        name="Upload"
+        component={Upload}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="cloud-upload" color={color} />,
+        }}
       />
       <Tab.Screen
         name="Profile"
         component={Profile}
-        options={{headerShown: false}}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="person" color={color} />,
+        }}
       />
     </Tab.Navigator>
   );
@@ -53,18 +44,7 @@ const TabScreen = () => {
 const StackScreen = () => {
   const {isLoggedIn} = useContext(MainContext);
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#77A6F7',
-        },
-        headerTintColor: 'black',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          fontFamily: 'LatoBold',
-        },
-      }}
-    >
+    <Stack.Navigator>
       {isLoggedIn ? (
         <>
           <Stack.Screen
@@ -72,15 +52,10 @@ const StackScreen = () => {
             component={TabScreen}
             options={{headerShown: false}}
           />
-          <Stack.Screen
-            name="Single"
-            component={Single}
-            options={({route}) => ({title: route.params.title})}
-            headerStyle={{backgroundColor: '#D3E3FC'}}
-          />
+          <Stack.Screen name="Single" component={Single} />
         </>
       ) : (
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login" component={Login}></Stack.Screen>
       )}
     </Stack.Navigator>
   );
--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
-const loginUrl = 'https://media.mw.metropolia.fi/wbma/login';
+const appId = 'wxs4ecdr5fv6gtyhui';
 
-export {baseUrl, uploadsUrl};
+export {baseUrl, uploadsUrl, appId};
